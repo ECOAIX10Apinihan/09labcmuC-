6b4106ae1dcbab341e79ad4ff86c8b3490212430
JJ <kongpopsattarach.edu@gmail.com>
2023-12-15 15:38:26 +0700

    Eiei draft

--- lab9_1.cpp
+++ lab9_1.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
 #include<iomanip> //For using setw(), setprecision(), ...
+#include<cmath>
+#include<string>
 using namespace std;
 
 int main(){	
@@ -10,7 +12,13 @@ int main(){
 	cin >> year;
 	cout << "Enter amount you can pay per year: ";
 	cin >> pay;
+	double interest,total,newbalance,Ey;
+	Ey = 1;
+	interest = (year/100)*loan;
+	total = (loan + interest);
+	newbalance = total - pay;
 
+	
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -24,14 +32,17 @@ int main(){
 	
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
+	
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
+	cout << setw(13) << left << Ey; 
+	cout << setw(13) << left << loan;
+	cout << setw(13) << left << interest;
+	cout << setw(13) << left << total;
+	cout << setw(13) << left << pay;
+	cout << setw(13) << left << newbalance;
 	cout << "\n";	
 	
 	return 0;
+
+
 }
